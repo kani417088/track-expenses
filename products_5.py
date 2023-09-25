8bfd82e7da5c2261c7edd9aa17706bb2f9acd39e
林佳穎 <kani417088@gmail.com>
2023-09-26 00:45:45 +0800

    revise mistake

--- products_5.py
+++ products_5.py
@@ -37,7 +37,7 @@ def print_products(products):
 
 # 寫入檔案
 def write_file(filename, products):
-	with open('filename', 'w', encoding = 'utf-8') as f: # w 是 write 寫入模式 # 可以存成 txt or csv
+	with open(filename, 'w', encoding = 'utf-8') as f: # w 是 write 寫入模式 # 可以存成 txt or csv
 		f.write('商品,價格\n') # 會亂碼因為編碼的問題, 要加上 encoding = 'utf-8' 此編碼可以讀也可以寫各種符號跟語言
 		for p in products:
 			f.write(p[0] + ',' + p[1] + '\n') # 4個字串加起來變成大字串, 丟回給f檔案做write
